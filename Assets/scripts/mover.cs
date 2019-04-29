724b7ca1e82d11650c957b4f9d0c051fa3f25ec5
Aman Almadi <amanalmadi23@gmail.com>
2019-04-30 00:28:28 +0530

    Add files via upload

---
+++ Assets/scripts/mover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mover : MonoBehaviour {
+
+    float x;
+    public float temp = 0.5f;
+   
+	// Use this for initialization
+	void Start () {
+        x = gameObject.transform.position.x;
+        gameObject.transform.position = new Vector2(15.0f, 2.5f);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if(gameObject.transform.position.x >= -15.0f)
+            gameObject.transform.position = new Vector2(x = x - temp, gameObject.transform.position.y);
+        if (gameObject.transform.position.x <= -15.0f)
+            Destroy(gameObject);
+    }
+}
